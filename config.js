70ff62e9fbfb7f7b870fc9d51af2647a0f4cdad3
minikiller@qq.com <minikiller@qq.com>
2020-01-21 20:27:13 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var mysql = require('mysql');
+
+module.exports = {
+    name: 'rest-api',
+    hostname : 'http://localhost',
+    version: '0.0.1',
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    db: {
+        get : mysql.createConnection({
+			host     : '192.119.116.101',
+			user     : 'root',
+			password : '123',
+			database : 'kalix'
+		})
+    }
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+## install mariadb
+```$xslt
+https://www.cnblogs.com/jianxuanbing/archive/2018/04/02/8693157.html
+```
+root password: kalix@123
+
+192.119.116.101
+
+``` 
+# mysql -u root -p
+MariaDB [(none)]> 
+grant all privileges on *.* to root@'%' identified by '123' with grant option;
+Query OK, 0 rows affected (0.00 sec)
+
+MariaDB [(none)]> 
+flush privileges;
+Query OK, 0 rows affected (0.01 sec)
+```
